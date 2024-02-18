2b6bcf338794b34bcba2730c70dcbb73ce97311b
Ottermandias <nickroederspam+git1@gmail.com>
2024-02-18 12:58:46 +0100

    Add temporary mod for mod setting change.

--- Enums/ModSettingChange.cs
+++ Enums/ModSettingChange.cs
@@ -5,33 +5,24 @@ namespace Penumbra.Api.Enums;
 /// </summary>
 public enum ModSettingChange
 {
-    /// <summary>
-    /// It was set to inherit from other collections or not to inherit anymore.
-    /// </summary>
+    /// <summary> It was set to inherit from other collections or not to inherit anymore. </summary>
     Inheritance,
 
-    /// <summary>
-    /// It was enabled or disabled.
-    /// </summary>
+    /// <summary> It was enabled or disabled. </summary>
     EnableState,
 
-    /// <summary>
-    /// Its priority was changed.
-    /// </summary>
+    /// <summary> Its priority was changed. </summary>
     Priority,
 
-    /// <summary>
-    /// A specific setting for an option group was changed.
-    /// </summary>
+    /// <summary> A specific setting for an option group was changed. </summary>
     Setting,
 
-    /// <summary>
-    /// Multiple mods were set to inherit from other collections or not inherit anymore at once.
-    /// </summary>
+    /// <summary> Multiple mods were set to inherit from other collections or not inherit anymore at once. </summary>
     MultiInheritance,
 
-    /// <summary>
-    /// Multiple mods were enabled or disabled at once.
-    /// </summary>
+    /// <summary> Multiple mods were enabled or disabled at once. </summary>
     MultiEnableState,
+
+    /// <summary> A temporary mod was enabled or disabled. </summary>
+    TemporaryMod,
 }
