34921fd2c5a9aff5d34aef664bdb78331e8b9436
Ottermandias <nickroederspam+git1@gmail.com>
2024-03-09 12:25:39 +0100

    Add 3-type action subscriber.

--- Helpers/ActionSubscriber.cs
+++ Helpers/ActionSubscriber.cs
@@ -85,3 +85,30 @@ public readonly struct ActionSubscriber<T1, T2>
     public void Invoke(T1 a, T2 b)
         => _subscriber?.InvokeAction(a, b);
 }
+
+/// <inheritdoc cref="ActionSubscriber"/> 
+public readonly struct ActionSubscriber<T1, T2, T3>
+{
+    private readonly ICallGateSubscriber<T1, T2, T3, object?>? _subscriber;
+
+    /// <inheritdoc cref="ActionSubscriber{T1}.Valid"/> 
+    public bool Valid
+        => _subscriber != null;
+
+    public ActionSubscriber(DalamudPluginInterface pi, string label)
+    {
+        try
+        {
+            _subscriber = pi.GetIpcSubscriber<T1, T2, T3, object?>(label);
+        }
+        catch (Exception e)
+        {
+            PluginLogHelper.WriteError(pi, $"Error registering IPC Subscriber for {label}\n{e}");
+            _subscriber = null;
+        }
+    }
+
+    /// <inheritdoc cref="ActionSubscriber.Invoke"/> 
+    public void Invoke(T1 a, T2 b, T3 c)
+        => _subscriber?.InvokeAction(a, b, c);
+}
